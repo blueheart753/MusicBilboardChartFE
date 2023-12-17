9959a98111e34865f9033b1f9f4b32ee5f26ff7f
kimminam <bluemind8520@gmail.com>
2023-12-17 15:41:23 +0900

    change url name

--- src/app/static/js/fetch.js
+++ src/app/static/js/fetch.js
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 const FetchModule = () => {
   const fetchData = async () => {
     try {
-      const response = await fetch("http://localhost:8080/apple"); // 프록시 설정을 통해 http://localhost:8080/ 로 요청이 전달됩니다.
+      const response = await fetch("http://localhost:8080/melon");
       const data = await response.json();
       console.log("Api : ", data);
     } catch (error) {
